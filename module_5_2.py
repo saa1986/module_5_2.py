cf10902712bf11abc10ebb858a4852ece105393a
saa1986 <1986zip@mail.ru>
2024-09-13 13:01:31 +0500

    Add files via upload

--- module_5_2.py
+++ module_5_2.py
@@ -13,6 +13,12 @@ class House:
     def display_info(self):  # Новый метод для отображения информации
         print(f"Название: {self.name}, кол-во этажей: {self.number_of_floors}")
 
+    def __len__(self):  # Метод для получения количества этажей
+        return self.number_of_floors
+
+    def __str__(self):  # Метод для получения строкового представления объекта
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
 # Создание объектов класса House
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
@@ -22,5 +28,9 @@ h1.display_info()
 h2.display_info()
 
 # Вывод количества этажей
-print(h1.number_of_floors)
-print(h2.number_of_floors)
+print(len(h1))  # Используем метод __len__
+print(len(h2))  # Используем метод __len__
+
+# Вывод строкового представления объектов
+print(h1)  # Используем метод __str__
+print(h2)  # Используем метод __str__
